349c9de9e143f9c8e9b829334e7e5b22fec594f3
Mangala Vinod <mangalajanardhan.k@gmail.com>
2022-05-11 18:17:06 -0400

    New Java file

--- MyFirstJava.java
+++ MyFirstJava.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class MyFirstJava {
+public class MyFirstJava {
 }
--- src/main/java/webDriver/SeleniumTest.java
+++ src/main/java/webDriver/SeleniumTest.java
@@ -1,2 +1,15 @@
-package webDriver;public class SeleniumTest {
+package webDriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args){
+        System.out.println("Welcome");
+        WebDriver driver= new ChromeDriver();
+
+
+
+    }
+
 }
