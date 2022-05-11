3ef1df37f32e87ec7e481e869df978c5ed88f543
Mangala Vinod <mangalajanardhan.k@gmail.com>
2022-05-11 17:59:28 -0400

    Committed my First.java file

--- src/main/java/org/example/MyFirstJava.java
+++ src/main/java/org/example/MyFirstJava.java
@@ -1,2 +1,24 @@
-package org.example;public class MyFirstJava {
+package org.example;
+
+public class MyFirstJava {
+    String firstName = null;
+
+    public MyFirstJava() {
+        firstName = new String("test");
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstNameParam) {
+        this.firstName = firstNameParam;
+    }
+
+    public static void main(String[] args) {
+        MyFirstJava myFirstJavaObject = new MyFirstJava();
+        myFirstJavaObject.setFirstName("mangala");
+        System.out.println(myFirstJavaObject.getFirstName());
+    }
 }
+
